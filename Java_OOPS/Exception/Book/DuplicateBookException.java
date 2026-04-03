b2a25a725712b5942740807e5d34591a9b446180
PGCP-BDA <PGCP-BDA@students.acts.in>
2026-04-03 15:09:08 +0530

    Exceptions Updated

---
+++ Java_OOPS/Exception/Book/DuplicateBookException.java
@@ -0,0 +1,7 @@
+package Git_Java.cdac_java_learning.Java_OOPS.Exception.Book;
+
+public class DuplicateBookException extends Exception {
+    public DuplicateBookException(String message){
+        super(message);
+    }  
+}
